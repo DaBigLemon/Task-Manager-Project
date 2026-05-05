47d65aa249c6ec5dd843059b857bb1b12f972e4d
DaBigLemon <liamhadfiel87@gmail.com>
2026-05-05 20:52:03 +1000

    finally finished seting up and made a quick while loop
    finished setting up vs code and github so it will work with my mac and windows pc now i can start developing the application
    
    in future i will need to add a way to actually store tasks, print them, and delete them

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 
 int main() {
+    bool quit = false;
 
-    std::cout << "hello my fellow friends!" << std::endl;
+    std::string userInput = "";
+
+    // SIMPLE WHILE LOOP JUST TO TEST IF EVERYTHINGS WORKING
+    std::cout << "Hello Welcome to my task manager CLI\ntype add, list, delete or quit"
+     << std::endl;
+    // Getting the bare bones framework for the Task Manager CLI so i can improve
+
+    // TODO: Need to be able to add tasks to a vector and able to print it to consol
+    while (quit == false){
+        std::cin >> userInput;
+
+        if (userInput == "add"){
+            std::cout << "\nyou added a Task!!!!\n" << std::endl;
+        }
+        else if (userInput == "list"){
+            std::cout << "\nyou Showed the list of tasks youve added!\n" << std::endl;
+        }
+        else if (userInput == "delete"){
+            std::cout << "\nyou deleted a task from the list\n" << std::endl;
+        }
+        else if (userInput == "quit"){
+            quit = true;
+        }
+        std::cout << "Please type\n"
+            << "add\n" << "list\n" << "delete\n" << "quit\n\n" << std::endl;
+             
+    } 
+    
 
 
 
