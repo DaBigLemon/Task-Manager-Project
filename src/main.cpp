94175a6f67c0635b56cd9ea06779d7ee27134d58
DaBigLemon <liamhadfiel87@gmail.com>
2026-05-07 00:31:27 +1000

    added adding and printing lists
    added printing and adding tasks. i also came accross a bug where the getline() feature would print a empty string but it is now fixed. next commit should hopefully be a delete feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+// COMPLETED: Need to be able to add tasks to a vector and able to print it to consol
+/*
+TODO: Make delete feature work. first just make it delete most recent and then after 
+let the user decide what task to delete
+*/
+
+void BorderLine();
 
 int main() {
     bool quit = false;
+    std::vector<std::string> taskListVec;
 
     std::string userInput = "";
 
@@ -10,15 +21,30 @@ int main() {
      << std::endl;
     // Getting the bare bones framework for the Task Manager CLI so i can improve
 
-    // TODO: Need to be able to add tasks to a vector and able to print it to consol
+    
     while (quit == false){
         std::cin >> userInput;
 
         if (userInput == "add"){
-            std::cout << "\nyou added a Task!!!!\n" << std::endl;
+            std::cout << "\nType the task you want to add!" << std::endl;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // clear leftover newline
+            std::getline(std::cin, userInput);
+            if (userInput == "quit"){
+                return 0;
+            }
+            else{
+                taskListVec.push_back(userInput);
+                std::cout << "You added " << userInput << " to the list!" << std::endl;
+            }
+
         }
         else if (userInput == "list"){
-            std::cout << "\nyou Showed the list of tasks youve added!\n" << std::endl;
+            std::cout << "\nTASKS YOU NEED TO COMPLETE!\n" << std::endl;
+            BorderLine();
+            for (int i = 0; i < taskListVec.size(); i++){
+                std::cout << "[" << i + 1 << "] " << taskListVec[i] << std::endl; 
+            }
+            BorderLine();
         }
         else if (userInput == "delete"){
             std::cout << "\nyou deleted a task from the list\n" << std::endl;
@@ -27,12 +53,14 @@ int main() {
             quit = true;
         }
         std::cout << "Please type\n"
-            << "add\n" << "list\n" << "delete\n" << "quit\n\n" << std::endl;
+            << "add, " << "list, " << "delete, " << "quit\n\n" << std::endl;
              
-    } 
+    }   
     
-
-
-
     return 0;
+}
+
+void BorderLine(){
+    std::cout << "\n**********************************************\n" << std::endl;
+
 }
